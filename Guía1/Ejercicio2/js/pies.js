640d4750c8d40534f1801a8b03c79c68c89aa73f
josebenjamin04 <67609517+josebenjamin04@users.noreply.github.com>
2021-07-19 11:18:22 -0600

    Add files via upload

---
+++ Guía1/Ejercicio2/js/pies.js
@@ -0,0 +1,10 @@
+
+const M = 1.0;
+const P = 3.28;
+var pies = prompt('Introduzca la unidad que desea convertir de metros a pies:','');
+var conversion;
+conversion = (pies*(P/M));
+document.write("<br>");
+document.write("<header><h2>Ingreso: " + pies + "m </h2><hr /><br/></header>");
+document.write("<header><h2>La unidad de conversion selecciono es de metros a pies </h2><hr /><br/></header>");
+document.write("<header><h2>La conversión de metros a pies es: " + area + "</h2><hr /><br/></header>");
---
+++ Guía1/Ejercicio2/js/pulgadas.js
@@ -0,0 +1,10 @@
+
+const M = 1.0;
+const Pul = 39.37;
+var pulgada = prompt('Introduzca la unidad que desea convertir de metros a pies:','');
+var conversion;
+conversion = (pulgada*(pUL/M));
+document.write("<br>");
+document.write("<header><h2>Ingreso: " + pulgada + "m </h2><hr /><br/></header>");
+document.write("<header><h2>La unidad de conversion selecciono es de metros a yardas </h2><hr /><br/></header>");
+document.write("<header><h2>La conversión de metros a pies es: " + area + "</h2><hr /><br/></header>");
