06f57a23d8c1f4e73e8b1e088e37e295e2af64c8
josebenjamin04 <67609517+josebenjamin04@users.noreply.github.com>
2021-07-20 11:48:32 -0600

    Add files via upload

---
+++ Guía1/Ejercicio3/js/celcius.js
@@ -0,0 +1,5 @@
+const F= 9/5;
+var celcius = prompt('Introduzca la la tempreatura en celcius para convertirla en Fahrenheit: ','');
+var conversion;
+conversion = (f*celcius)+32;
+document.write("<header><h2>La conversion de celcius a fahrenheit es de:" + conversion + " fahrenheit </h2><hr /><br/></header>");
