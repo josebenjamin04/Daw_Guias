8ecbd9abc392ab6661e3900b238bfe10893a1302
josebenjamin04 <67609517+josebenjamin04@users.noreply.github.com>
2021-07-20 19:38:06 -0600

    Add files via upload

---
+++ Guía1/Ejercicio1/js/estructura.js
@@ -0,0 +1,6 @@
+document.write("<header><h1>HEADER</h1><hr /><br/></header>");
+document.write("<nav><h1>NAVEGATION</h1><hr /><br/></nav>");
+document.write("<article id='primero'><h1>ARTICULE 1</h1><hr /><br/></article>");
+document.write("<article id='segundo'><h1>ARTICULE 2</h1><hr /><br/></article>");
+document.write("<article id='tercero'><h1>ARTICULE 3</h1><hr /><br/></article>");
+document.write("<footer><h1>FOOTER</h1><hr /><br/></footer>");
